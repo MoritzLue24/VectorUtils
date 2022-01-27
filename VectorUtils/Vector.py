8dbd50f53b2472186d7cf4a2048c4a7184cebff7
Monkvy <moluecke@gmx.de>
2022-01-28 00:28:30 +0100

    kn

--- VectorUtils/Vector.py
+++ VectorUtils/Vector.py
@@ -1,9 +1,25 @@
+from math import sqrt, atan2, hypot
 
 
 class Vector2:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def getMag(self):
+        return sqrt(self.x**2 + self.y**2)
+
+    def setMag(self, magnitude):
+        newX = self.x * magnitude / self.getMag()
+        newY = self.y * magnitude / self.getMag()
+        self.x = newX
+        self.y = newY
+
+    def getAngle(self, other):
+        return atan2((other.y - self.y), (other.x - self.x))
+
+    def getDist(self, other):
+        return hypot((other.x - self.x), (other.y - self.y))
 
     def toTuple(self):
         return self.x, self.y
