131d831fb15c17dce5790c359c703ddc77baab12
Monkvy <moluecke@gmx.de>
2022-08-29 21:05:11 +0200

    Removed dot product

--- VectorUtils/__init__.py
+++ VectorUtils/__init__.py
@@ -1,2 +1,1 @@
 from VectorUtils.vector2 import *
-from VectorUtils.utils import *
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '2.1.3'
+VERSION = '2.1.4'
 DESCRIPTION = 'Visit https://github.com/Monkvy/VectorUtils for the source code.'
 
 # Setting up
